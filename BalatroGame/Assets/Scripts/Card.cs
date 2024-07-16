07cbcdd54a902f6aeb9fae7f5f8d074da84c6c5e
Shay yosef <shayagres@gmail.com>
2024-07-16 17:47:26 +0300

    Add Card Sprites

--- BalatroGame/Assets/Scripts/Card.cs
+++ BalatroGame/Assets/Scripts/Card.cs
@@ -8,6 +8,20 @@ using UnityEngine.UI;
 
 public class Card : MonoBehaviour
 {
+    public Image image;
+    public Sprite _sprite;
     public string _suit;
     public string _rank;
+
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+    }
+
+    private void Start()
+    {
+    //    _sprite = Resources.Load<Sprite>("Cards/" + _suit + "_" + _rank);
+        image.sprite = Resources.Load<Sprite>("Cards/" + _suit + "_" + _rank);
+        //Debug.Log("Loaded: " + _suit + "_" + _rank);
+    }
 }
