bb6b6ba86be4f488ba245ab09c5c793c7ffe2888
Shay yosef <shayagres@gmail.com>
2024-07-21 15:05:40 +0300

    Change Poker System

--- BalatroGame/Assets/Scripts/PokerSystem.cs
+++ BalatroGame/Assets/Scripts/PokerSystem.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using System;
+
 public enum HandType
 {
     HighCard,
@@ -14,6 +17,7 @@ public enum HandType
     StraightFlush,
     RoyalFlush
 }
+
 public class PokerSystem : MonoBehaviour
 {
     private static PokerSystem _instance;
@@ -21,7 +25,6 @@ public class PokerSystem : MonoBehaviour
     public HandType _handType;
 
     public static PokerSystem Instance => _instance;
-
 
     private void Awake()
     {
@@ -89,249 +92,7 @@ public class PokerSystem : MonoBehaviour
         }
     }
 
-
-
-    public bool IsPair(List<Card> handToCheck)
-    {
-        for (int i = 0; i < handToCheck.Count; i++)
-        {
-            for (int j = i + 1; j < handToCheck.Count; j++)
-            {
-                if (handToCheck[i].Rank == handToCheck[j].Rank)
-                {
-                    ListsManager.Instance.UpdateScoredCards(handToCheck[i]);
-                    ListsManager.Instance.UpdateScoredCards(handToCheck[j]);
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    public bool IsTwoPair(List<Card> handToCheck)
-    {
-        Dictionary<string, int> rankCount = new Dictionary<string, int>();
-        foreach (var card in handToCheck)
-        {
-            if (rankCount.ContainsKey(card.Rank))
-            {
-                rankCount[card.Rank]++;
-                ListsManager.Instance.UpdateScoredCards(card);
-                ListsManager.Instance.UpdateScoredCards(card);
-            }
-            else
-            {
-                rankCount[card.Rank] = 1;
-            }
-        }
-
-        int pairs = 0;
-        foreach (var count in rankCount.Values)
-        {
-            if (count == 2)
-            {
-                pairs++;
-            }
-        }
-
-        return pairs == 2;
-    }
-
-
-    public bool IsThreeOfAKind(List<Card> handToCheck)
-    {
-        for (int i = 0; i < handToCheck.Count; i++)
-        {
-            for (int j = i + 1; j < handToCheck.Count; j++)
-            {
-                for (int k = j + 1; k < handToCheck.Count; k++)
-                {
-                    if (handToCheck[i].Rank == handToCheck[j].Rank && handToCheck[j].Rank == handToCheck[k].Rank)
-                    {
-                        ListsManager.Instance.UpdateScoredCards(handToCheck[i]);
-                        ListsManager.Instance.UpdateScoredCards(handToCheck[j]);
-                        ListsManager.Instance.UpdateScoredCards(handToCheck[k]);
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
-    }
-
-    public bool IsStraight(List<Card> handToCheck)
-    {
-        List<int> ranks = new List<int>();
-        foreach (Card card in handToCheck)
-        {
-            switch (card.Rank)
-            {
-                case "2":
-                    ranks.Add(2);
-                    break;
-                case "3":
-                    ranks.Add(3);
-                    break;
-                case "4":
-                    ranks.Add(4);
-                    break;
-                case "5":
-                    ranks.Add(5);
-                    break;
-                case "6":
-                    ranks.Add(6);
-                    break;
-                case "7":
-                    ranks.Add(7);
-                    break;
-                case "8":
-                    ranks.Add(8);
-                    break;
-                case "9":
-                    ranks.Add(9);
-                    break;
-                case "10":
-                    ranks.Add(10);
-                    break;
-                case "J":
-                    ranks.Add(11);
-                    break;
-                case "Q":
-                    ranks.Add(12);
-                    break;
-                case "K":
-                    ranks.Add(13);
-                    break;
-                case "A":
-                    ranks.Add(14);
-                    ranks.Add(1); // Ace can also be low
-                    break;
-            }
-        }
-        ranks.Sort();
-        // Check for a sequence of exactly 5 cards
-        for (int i = 0; i <= ranks.Count - 5; i++)
-        {
-            if (ranks[i] + 1 == ranks[i + 1] &&
-                ranks[i + 1] + 1 == ranks[i + 2] &&
-                ranks[i + 2] + 1 == ranks[i + 3] &&
-                ranks[i + 3] + 1 == ranks[i + 4])
-            {
-                ListsManager.Instance.UpdateScoredCards(handToCheck[i]);
-                ListsManager.Instance.UpdateScoredCards(handToCheck[i + 1]);
-                ListsManager.Instance.UpdateScoredCards(handToCheck[i + 2]);
-                ListsManager.Instance.UpdateScoredCards(handToCheck[i + 3]);
-                ListsManager.Instance.UpdateScoredCards(handToCheck[i + 4]);
-                return true;
-            }
-        }
-        return false;
-    }
-
-
-    public bool IsFlush(List<Card> handToCheck)
-    {
-        if (handToCheck.Count < 5)
-        {
-            return false; // Not enough cards to form a flush
-        }
-        else if (handToCheck.Count == 5)
-        {
-            // Check if all cards have the same suit
-            string suit = handToCheck[0].Suit;
-            foreach (var card in handToCheck)
-            {
-                ListsManager.Instance.UpdateScoredCards(card);
-                if (card.Suit != suit)
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
-        return false;
-    }
-
-
-    public bool IsFullHouse(List<Card> handToCheck)
-    {
-        Dictionary<string, int> rankCount = new Dictionary<string, int>();
-        foreach (var card in handToCheck)
-        {
-            if (rankCount.ContainsKey(card.Rank))
-            {
-                rankCount[card.Rank]++;
-            }
-            else
-            {
-                rankCount[card.Rank] = 1;
-            }
-        }
-
-        bool hasThreeOfAKind = false;
-        bool hasPair = false;
-
-        foreach (var count in rankCount.Values)
-        {
-            if (count == 3)
-            {
-                hasThreeOfAKind = true;
-            }
-            if (count == 2)
-            {
-                hasPair = true;
-            }
-        }
-
-        return hasThreeOfAKind && hasPair;
-    }
-
-
-    public bool IsFourOfAKind(List<Card> handToCheck)
-    {
-        for (int i = 0; i < handToCheck.Count; i++)
-        {
-            for (int j = i + 1; j < handToCheck.Count; j++)
-            {
-                for (int k = j + 1; k < handToCheck.Count; k++)
-                {
-                    for (int l = k + 1; l < handToCheck.Count; l++)
-                    {
-                        if (handToCheck[i].Rank == handToCheck[j].Rank && handToCheck[j].Rank == handToCheck[k].Rank && handToCheck[k].Rank == handToCheck[l].Rank)
-                        {
-                            return true;
-                        }
-                    }
-                }
-            }
-        }
-        return false;
-    }
-
-    public bool IsStraightFlush(List<Card> handToCheck)
-    {
-        if (handToCheck.Count < 5)
-        {
-            return false;
-        }
-
-        // Convert ranks to integer values and sort the hand
-        List<Card> sortedHand = new List<Card>(handToCheck);
-        sortedHand.Sort((a, b) => GetRankValue(a.Rank).CompareTo(GetRankValue(b.Rank)));
-
-        for (int i = 0; i <= sortedHand.Count - 5; i++)
-        {
-            List<Card> potentialStraightFlush = sortedHand.GetRange(i, 5);
-            if (IsFlush(potentialStraightFlush) && IsStraight(potentialStraightFlush))
-            {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    private int GetRankValue(string rank)
+    private int GetRankValue(string rank, bool aceAsOne = false)
     {
         switch (rank)
         {
@@ -347,32 +108,99 @@ public class PokerSystem : MonoBehaviour
             case "J": return 11;
             case "Q": return 12;
             case "K": return 13;
-            case "A": return 14;
-            default: return 0; // Handle invalid rank if necessary
+            case "A": return aceAsOne ? 1 : 14;
+            default: throw new ArgumentException("Invalid card rank");
         }
     }
 
     public bool IsRoyalFlush(List<Card> handToCheck)
+    {
+        if (handToCheck.Count != 5)
+        {
+            return false;
+        }
+
+        if (!IsFlush(handToCheck))
+        {
+            return false;
+        }
+
+        HashSet<string> royalRanks = new HashSet<string> { "10", "J", "Q", "K", "A" };
+        return handToCheck.All(card => royalRanks.Contains(card.Rank));
+    }
+
+    public bool IsPair(List<Card> handToCheck)
+    {
+        return handToCheck.GroupBy(card => card.Rank).Any(group => group.Count() == 2);
+    }
+
+    public bool IsTwoPair(List<Card> handToCheck)
+    {
+        return handToCheck.GroupBy(card => card.Rank).Count(group => group.Count() == 2) == 2;
+    }
+
+    public bool IsThreeOfAKind(List<Card> handToCheck)
+    {
+        return handToCheck.GroupBy(card => card.Rank).Any(group => group.Count() == 3);
+    }
+
+    public bool IsStraight(List<Card> handToCheck)
+    {
+        List<int> ranks = handToCheck.Select(card => GetRankValue(card.Rank)).Distinct().OrderBy(rank => rank).ToList();
+        if (ranks.Count < 5)
+        {
+            return false;
+        }
+
+        // Check for standard straight
+        for (int i = 0; i <= ranks.Count - 5; i++)
+        {
+            if (ranks[i] + 1 == ranks[i + 1] &&
+                ranks[i + 1] + 1 == ranks[i + 2] &&
+                ranks[i + 2] + 1 == ranks[i + 3] &&
+                ranks[i + 3] + 1 == ranks[i + 4])
+            {
+                return true;
+            }
+        }
+
+        // Check for Ace-low straight
+        if (ranks.Contains(14) && ranks.Contains(2) && ranks.Contains(3) && ranks.Contains(4) && ranks.Contains(5))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public bool IsFlush(List<Card> handToCheck)
     {
         if (handToCheck.Count < 5)
         {
             return false;
         }
 
-        // Convert ranks to integer values and sort the hand
-        List<Card> sortedHand = new List<Card>(handToCheck);
-        sortedHand.Sort((a, b) => GetRankValue(a.Rank).CompareTo(GetRankValue(b.Rank)));
-
-        for (int i = 0; i <= sortedHand.Count - 5; i++)
-        {
-            List<Card> potentialRoyalFlush = sortedHand.GetRange(i, 5);
-            if (IsFlush(potentialRoyalFlush) && IsStraight(potentialRoyalFlush) && GetRankValue(potentialRoyalFlush[0].Rank) == 10)
-            {
-                return true;
-            }
-        }
-
-        return false;
+        string suit = handToCheck[0].Suit;
+        return handToCheck.All(card => card.Suit == suit);
     }
 
+    public bool IsFullHouse(List<Card> handToCheck)
+    {
+        var groupedRanks = handToCheck.GroupBy(card => card.Rank).ToList();
+        bool hasThreeOfAKind = groupedRanks.Any(group => group.Count() == 3);
+        bool hasPair = groupedRanks.Any(group => group.Count() == 2);
+        return hasThreeOfAKind && hasPair;
+    }
+
+    public bool IsFourOfAKind(List<Card> handToCheck)
+    {
+        return handToCheck.GroupBy(card => card.Rank).Any(group => group.Count() == 4);
+    }
+
+    public bool IsStraightFlush(List<Card> handToCheck)
+    {
+        return IsFlush(handToCheck) && IsStraight(handToCheck);
+    }
 }
+
+
